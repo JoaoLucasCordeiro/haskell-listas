f27eef50e96740db0dbc6cfcced20416e2c0cc8b
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2024-05-15 10:46:25 -0300

    feat: comments

--- q1.hs
+++ q1.hs
@@ -1,3 +1,6 @@
+-- Usando compreensão de listas, forneça uma expressão que calcula 
+-- a soma 1² +2²+...+100² dos quadrados dos primeiros 100 números inteiros.
+
 main :: IO ()
 main = do
     let sumOfSquares = sum [x^2 | x <- [1..100]]
